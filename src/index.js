5d06aedd85b5d2c676d012b485204d6dc4fd8bfc
Carlos Martinez <martinezcj2@gmail.com>
2017-09-21 00:45:24 -0400

    Apply promise to middleware

--- src/index.js
+++ src/index.js
@@ -3,26 +3,19 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/posts_index';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
-class Hello extends React.Component {
-  render() { return ( <div>Hello</div> ) }
-}
-
-class Goodbye extends React.Component {
-  render() { return ( <div>Goodbye</div> ) }
-}
-
+// http://reduxblog.herokuapp.com/
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/hello" component={Hello} /> 
-        <Route path="/goodbye" component={Goodbye} />
+       <Route path='/' component={PostIndex} />
       </div>
      </BrowserRouter>
   </Provider>
