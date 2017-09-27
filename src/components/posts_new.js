ea9a07961a2ca6aebf13e383c94b5f80e8040233
Carlos Martinez <martinezcj2@gmail.com>
2017-09-27 13:51:36 -0400

    Add validation to inputs

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,75 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-  render() {
+  renderField(field) {
+    const { touched, error } = field.meta;
+    const className = `form-group ${ touched && error ? 'has-danger' : '' }`
+    
     return (
-      <div>
-        Posts New
+      <div className={className}>
+        <label>{field.label}</label>
+        <input 
+          className='form-control'
+          type="text" 
+          {...field.input}
+        />
+        <div className='text-help'>
+          { touched ? error : '' }
+        </div>
       </div>
+    )
+  }
+
+  onSubmit(values) {
+    console.log(values);
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field
+          label='Title'
+          name='title'
+          component={this.renderField}
+        />
+        <Field
+          label='Categories'
+          name='categories'
+          component={this.renderField}
+        />
+        <Field
+          label='Content'
+          name='content'
+          component={this.renderField}
+        />
+        <button type='submit' className='btn btn-primary'>Submit</button>
+      </form>
     )
   }
 }
 
-export default PostsNew;
+function validate(values) {
+  const errors = {};
+
+  if(!values.title) {
+    errors.title = 'Enter title!';
+  }
+
+  if(!values.categories) {
+    errors.categories = 'Enter some categories!';
+  }
+  
+  if(!values.content) {
+    errors.content = 'Enter some content!';
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostNewForm'
+})(PostsNew);
