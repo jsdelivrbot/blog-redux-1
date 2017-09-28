520172bfda1f81f847f1a2d7a619620ff6dbe9dd
Carlos Martinez <martinezcj2@gmail.com>
2017-09-28 03:40:55 -0400

    Add case for delete post

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import { FETCH_POSTS, FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function(state = {}, action) {
   switch (action.type) {
@@ -8,6 +8,8 @@ export default function(state = {}, action) {
   case FETCH_POST:
     const post = action.payload.data;
     return { ...state, [action.payload.data.id]: action.payload.data }; //adding key and value to new state object
+  case DELETE_POST:
+    return _.omit(state, action.payload) //returns new state without id passed in action  
   default:
     return state;
   }
